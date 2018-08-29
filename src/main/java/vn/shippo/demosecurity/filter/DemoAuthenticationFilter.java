6b5ef2515e666375487303f17ce440dfb3bfde83
tuanpmt38 <tuanpmt38@gmail.com>
2018-08-29 11:24:56 +0700

    fix

--- src/main/java/vn/shippo/demosecurity/filter/DemoAuthenticationFilter.java
+++ src/main/java/vn/shippo/demosecurity/filter/DemoAuthenticationFilter.java
@@ -31,7 +31,6 @@ public class DemoAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-
         /** check the request true or false*/
         if (request instanceof HttpServletRequest == false) {
             filterChain.doFilter(request, response);
